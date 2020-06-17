90f7ea6756ce320b768ef6baef1687e82c2a529b
andreubita <andre.vaz1411@gmail.com>
2020-06-17 17:37:03 +0100

    chrome-dino INIT

---
+++ chrome-dino/sketch.js
@@ -0,0 +1,21 @@
+var canvas;
+
+let grapeImg;
+let backgroundImg;
+let 
+
+function preload(){
+
+}
+
+function setup(){
+    let halfWindowWidth = windowWidth / 2;
+    let halfWindowHeight = windowHeight / 2;
+    
+    canvas = createCanvas(1200, 500);
+    canvas.position(halfWindowWidth - width / 2,  halfWindowHeight - height / 2);
+}
+
+function draw(){
+    background(0);
+}
